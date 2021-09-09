e19ade4b5fe353a9e1b598b345c91931a82ae6a6
Daniel Cruz <adm.dcf@gmail.com>
2021-09-09 19:08:36 -0300

    Add Field tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,7 +1,9 @@
 import pytest
 
 from validatable.fields import Field
+from validatable.main import BaseTable
 from sqlalchemy import ForeignKey
+import sqlalchemy as sa
 
 
 @pytest.mark.parametrize(
@@ -27,3 +29,61 @@ def test_sa_foreign_key():
     field = Field(sa_foreign_key=fk)
 
     assert field.extra["sa_args"] == [fk]
+
+
+def test_model_primary_key():
+
+    metadata = sa.MetaData()
+
+    class Item(BaseTable, metadata=metadata):
+        id: int = Field(sa_primary_key=True)
+        name: str = Field("")
+
+    assert Item.c.id.primary_key
+
+
+def test_model_nullable():
+
+    metadata = sa.MetaData()
+
+    class Item(BaseTable, metadata=metadata):
+        id: int = Field(sa_primary_key=True)
+        name: str = Field("", sa_nullable=False)
+
+    assert not Item.c.name.nullable
+
+
+def test_model_index():
+
+    metadata = sa.MetaData()
+
+    class Item(BaseTable, metadata=metadata):
+        id: int = Field(sa_primary_key=True)
+        name: str = Field("", sa_index=True)
+
+    assert Item.c.name.index
+
+
+def test_model_unique():
+
+    metadata = sa.MetaData()
+
+    class Item(BaseTable, metadata=metadata):
+        id: int = Field(sa_primary_key=True)
+        name: str = Field("", sa_unique=True)
+
+    assert Item.c.name.unique
+
+
+def test_model_foreign_key():
+
+    metadata = sa.MetaData()
+
+    fk = ForeignKey("test.id")
+
+    class Item(BaseTable, metadata=metadata):
+        id: int = Field(sa_primary_key=True)
+        name: str = Field("", sa_foreign_key=fk)
+
+    assert len(Item.c.name.foreign_keys) == 1
+    assert fk in Item.c.name.foreign_keys
