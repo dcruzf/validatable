e4501c46ad8680eb5edc5c1d0bea680865113090
Daniel Cruz <adm.dcf@gmail.com>
2021-09-15 22:00:36 -0300

    :gift: Add commit message script

---
+++ commit.sh
@@ -0,0 +1,31 @@
+menu_from_array ()
+{
+select item; do
+# Check the selected menu item number
+if [ 1 -le "$REPLY" ] && [ "$REPLY" -le $# ];
+then
+break;
+else
+echo "Wrong selection ($REPLY): Select any number from 1-$#"
+fi
+done
+}
+
+GITEMOJIS=(':gift:,🎁,Add'
+           ':zap:,⚡,Update'
+           ':fire:,🔥,Delete'
+           ':bug:,🐛,Fix'
+           ':gear:,⚙️.,CI'
+           ':hammer_and_wrench:,🛠️.,Refactor'
+           ':heavy_check_mark:,✔️.,Test'
+           ':orange_book:,📙,Docs'
+           ':seedling:,🌱,Create')
+
+# Call the subroutine to create the menu
+ITEMS=()
+for I in ${GITEMOJIS[@]}; do ITEMS+=("$(echo -n $I | cut --output-delimiter=' ' -d ',' -f 2,3)"); done;
+menu_from_array "${ITEMS[@]}"
+
+read -p "$(echo -n ${GITEMOJIS[REPLY-1]} | cut --output-delimiter=' ' -d ',' -f 2,3) " msg
+
+git commit -m "$(echo -n ${GITEMOJIS[REPLY-1]} | cut --output-delimiter=' ' -d ',' -f 1,3) $msg"
