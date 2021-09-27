60e34071d1fc5e17106ae2d821ca4a857e211c12
Daniel Cruz <adm.dcf@gmail.com>
2021-09-27 18:21:38 -0300

    :heavy_check_mark: test: add generic types tests

---
+++ tests/test_generic_types.py
@@ -0,0 +1,195 @@
+from sqlalchemy.dialects import mysql, postgresql, sqlite
+from sqlalchemy.schema import Column, CreateColumn
+
+from validatable.generic_types import GUID, AutoString
+from uuid import UUID
+import pytest
+
+
+def test_guid_type_DDL_postgresql():
+    c = Column("c", GUID)
+    t_ddl = CreateColumn(c)
+
+    postgresql_ddl = str(t_ddl.compile(dialect=postgresql.dialect()))
+    _, postgresql_type = postgresql_ddl.split()
+
+    assert "UUID" == postgresql_type
+
+
+def test_guid_type_DDL_sqlite():
+    c = Column("c", GUID)
+    t_ddl = CreateColumn(c)
+
+    sqlite_ddl = str(t_ddl.compile(dialect=sqlite.dialect()))
+    _, sqlite_type = sqlite_ddl.split()
+
+    assert "BINARY(16)" == sqlite_type
+
+
+def test_guid_type_DDL_mysql():
+    c = Column("c", GUID)
+    t_ddl = CreateColumn(c)
+
+    mysql_ddl = str(t_ddl.compile(dialect=mysql.dialect()))
+    _, mysql_type = mysql_ddl.split()
+
+    assert "BINARY(16)" == mysql_type
+
+
+def test_guid_python_type():
+    assert GUID().python_type == UUID
+
+
+@pytest.mark.parametrize(
+    ["value", "dialect", "expected"],
+    (
+        (
+            [
+                UUID("08e6ff8e253545d584558a50a57667c5"),
+                "postgresql",
+                UUID("08e6ff8e253545d584558a50a57667c5"),
+            ],
+            [
+                None,
+                "postgresql",
+                None,
+            ],
+            [
+                UUID("08e6ff8e253545d584558a50a57667c5"),
+                "Any other",
+                UUID("08e6ff8e253545d584558a50a57667c5").bytes,
+            ],
+            [
+                None,
+                "Any other",
+                None,
+            ],
+        )
+    ),
+)
+def test_guid_process_bind_param(value, dialect, expected):
+    DialectMock = type("DialectMock", (object,), {"name": dialect})
+    guid = GUID()
+    result = guid.process_bind_param(value, DialectMock)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ["value", "dialect", "expected"],
+    (
+        (
+            [
+                UUID("08e6ff8e253545d584558a50a57667c5"),
+                "postgresql",
+                UUID("08e6ff8e253545d584558a50a57667c5"),
+            ],
+            [
+                None,
+                "postgresql",
+                None,
+            ],
+            [
+                UUID("08e6ff8e253545d584558a50a57667c5").bytes,
+                "Any other",
+                UUID("08e6ff8e253545d584558a50a57667c5"),
+            ],
+            [
+                None,
+                "Any other",
+                None,
+            ],
+        )
+    ),
+)
+def test_guid_process_result_value(value, dialect, expected):
+    DialectMock = type("DialectMock", (object,), {"name": dialect})
+    guid = GUID()
+    result = guid.process_result_value(value, DialectMock)
+    assert result == expected
+
+
+def test_autostring_python_type():
+    assert AutoString().python_type == str
+
+
+def test_autostring_type_DDL_postgresql():
+    c = Column("c", AutoString)
+    t_ddl = CreateColumn(c)
+
+    postgresql_ddl = str(t_ddl.compile(dialect=postgresql.dialect()))
+    _, postgresql_type = postgresql_ddl.split()
+
+    assert "VARCHAR" == postgresql_type
+
+
+def test_autostring_type_DDL_sqlite():
+    c = Column("c", AutoString)
+    t_ddl = CreateColumn(c)
+
+    sqlite_ddl = str(t_ddl.compile(dialect=sqlite.dialect()))
+    _, sqlite_type = sqlite_ddl.split()
+
+    assert "VARCHAR" == sqlite_type
+
+
+def test_autostring_type_DDL_mysql():
+    c = Column("c", AutoString)
+    t_ddl = CreateColumn(c)
+
+    mysql_ddl = str(t_ddl.compile(dialect=mysql.dialect()))
+    _, mysql_type = mysql_ddl.split()
+
+    assert "VARCHAR(512)" == mysql_type
+
+
+@pytest.mark.parametrize(
+    ["value", "dialect", "expected"],
+    (
+        (
+            [
+                "SomeString",
+                "Any",
+                "SomeString",
+            ],
+            [
+                None,
+                "Any",
+                None,
+            ],
+            [
+                1,
+                "Any",
+                "1",
+            ],
+        )
+    ),
+)
+def test_autostring_process_bind_param(value, dialect, expected):
+    DialectMock = type("DialectMock", (object,), {"name": dialect})
+    auto_string = AutoString()
+    result = auto_string.process_bind_param(value, DialectMock)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ["value", "dialect", "expected"],
+    (
+        (
+            [
+                "SomeString",
+                "Any",
+                "SomeString",
+            ],
+            [
+                None,
+                "Any",
+                None,
+            ],
+        )
+    ),
+)
+def test_autostring_process_result_value(value, dialect, expected):
+    DialectMock = type("DialectMock", (object,), {"name": dialect})
+    auto_string = AutoString()
+    result = auto_string.process_result_value(value, DialectMock)
+    assert result == expected
